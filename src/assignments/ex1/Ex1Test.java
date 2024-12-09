c968365761310a630ec4b091db69995ab1cbd1b4
Reuven Herszkowicz <reuvenal0@outlook.com>
2024-12-09 13:00:00 +0200

    first

--- src/assignments/ex1/Ex1Test.java
+++ src/assignments/ex1/Ex1Test.java
@@ -24,26 +24,19 @@ public class Ex1Test {
             assertTrue(Ex1.equals(s10,s2));
 
             //my testing:
-            String arrS [][] = {{"0b2", "0", "2bG"} , {"20CbE", "404", "EbG"}, {"153b7", "87" , "7bG"}, {"101", "101", "AbG"},{"AbC" , "10", "CbG"}};
+            String FirstTest [][] = {{"0b2", "0"} , {"20CbE", "404"}, {"153b7", "87"}, {"101", "101"},{"AbC" , "10"}};
 
-            int value, base;
-            String temp;
-            for (int i = 0; i < arrS.length; i++)
+            for (int i = 0; i < FirstTest.length; i++)
             {
                 //testing "number2Int" using "equals" function:
-                assertTrue(Ex1.equals(arrS[i][0], arrS[i][1]));
-
-                // testing "int2number"
-                v = Ex1.number2Int(arrS[i][1]); //decimal number form string to int
-                v2 = Ex1.number2Int(arrS[i][2]); //original base form string to int
-                s2 = Ex1.int2Number(v,v2); // number in original base (str)
-
-                assertEquals(arrS[i][0], s2);
+                assertTrue(Ex1.equals(FirstTest[i][0], FirstTest[i][1]));
             }
         }
 
         @Test
         void isBasisNumberTest() {
+            // added a few of me own Strings...
+
             String[] good = {"1", "1b2", "01b2", "123bA", "ABbG", "0bA", "1011b2", "1011bA", "00b2", "0000bG", "DDAbG"};
             for (int i=0;i<good.length; i++) {
                 boolean ok = Ex1.isNumber(good[i]);
@@ -57,38 +50,52 @@ public class Ex1Test {
         }
         @Test
         void int2NumberTest() {
-            //
+            String thirdTest [][] = {{"0b2", "0", "2bG"} , {"20CbE", "404", "EbG"}, {"153b7", "87" , "7bG"}, {"101", "101", "AbG"},{"AbC" , "10", "CbG"} ,{"15678b9", "10763", "9bG"}};
+            int value, base;
+            String temp;
+            for (int i = 0; i < thirdTest.length; i++)
+            {
+                // testing "int2number" using "number2int"
+                value = Ex1.number2Int(thirdTest[i][1]); //decimal number form string to int
+                base = Ex1.number2Int(thirdTest[i][2]); //original base form string to int
+                temp = Ex1.int2Number(value,base); // number in original base (str)
 
-            String num = "15678b9"; // this is check num/
-
-            String [] str_Numbers = num.split("b");
-            int Old_base = Integer.parseInt(str_Numbers[1],17);
-            int ans = Integer.parseInt(str_Numbers[0],Old_base);
-
-
-            assertEquals(Ex1.number2Int(num), ans);
-           // implement this test
-        }
-        @Test
-        void maxIndexTest() {
-            String[] NumArr = {"1","123b6", "ABbG", "0bA", "1011b2", "1011bA"};
-            for (int i=0;i<NumArr.length; i++) {
-                int max = Ex1.maxIndex(NumArr);
-                assertEquals(max, 1011);
+                assertEquals(thirdTest[i][0], temp);
             }
         }
 
+        @Test
+        void maxIndexTest() {
+            String[] NumArr = {"1" ,"123b6", "ABbG", "0bA", "1011b2", "1011bA", "404", "-2001"};
+
+            int max = Ex1.maxIndex(NumArr);
+            assertEquals(NumArr[max], "1011bA");
+        }
 
         @Test
         void isEqualsTest() {
-//            String [] ArrStr = {"hi this is me", "hi this is me"};
-//            boolean ok = Ex1.equals(ArrStr[0], ArrStr[1]);
-//            assertTrue(ok);
-//
-//            ArrStr = new String[]{"hi this is me", "hi this isn't me"};
-//            ok = Ex1.equals(ArrStr[0], ArrStr[1]);
-//            assertFalse(ok);
+            String [] arr1 = {"101", "65bG", "1100101b2" , "203b7" , "73bE" , "101bA"};
+            for (int i = 1; i < arr1.length; i++)
+            {
+                boolean ok = Ex1.equals(arr1[i - 1], arr1[i]);
+                assertTrue(ok);
+            }
+
+            String [] arr2 = {"101", "200bG", "56b7", "88bF" , "2001bA"};
+            for (int i = 1; i < arr2.length; i++)
+            {
+                boolean not_ok = Ex1.equals(arr2[i - 1], arr2[i]);
+                assertFalse(not_ok);
+            }
         }
 
-        // Add additional test functions - test as much as you can.
+    @Test
+    void chars_test() {
+//        String[] NumArr = {"1" ,"123b6", "ABbG", "0bA", "1011b2", "1011bA", "404", "-2001"};
+//
+//        int max = Ex1.maxIndex(NumArr);
+//        assertEquals(NumArr[max], "1011bA");
     }
+
+
+}
