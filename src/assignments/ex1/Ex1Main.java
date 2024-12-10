649b64506aeea42788dffb25815759bd506b8963
Reuven Herszkowicz <reuvenal0@outlook.com>
2024-12-10 13:52:43 +0200

    more changes

--- src/assignments/ex1/Ex1Main.java
+++ src/assignments/ex1/Ex1Main.java
@@ -32,8 +32,7 @@ public class Ex1Main {
                 //let's print what is the status of num1: valid and decimal value
                 System.out.println("num1= " + num1 + " is number: "+ validNum1 + " , value: " + num1_value);
 
-                // in case of invalid input:
-                if (!validNum1)
+                if (!validNum1) // in case of invalid input to num1:
                 {
                     System.out.println("ERR: num1 is in the wrong format! (" + num1+ ")");
                 }
@@ -41,52 +40,67 @@ public class Ex1Main {
                 // in case of valid num1 input:
                 else if (validNum1)
                 {
+                    // moving on to asking the user second number:
+
                     //msg print to the user
                     System.out.println("Enter a string as number#2 (or \"quit\" to end the program): ");
 
                     // getting Second number form the user: num2
                     num2 = sc.next();
 
-                    // if the user input "quit", let's restart and ask again for first number,
-                    // so let's break this loop iteration:
+                    // if the user input "quit", we need to close the program, so let's break this loop:
                     if (num2.equals("quit")) break;
 
                     boolean validNum2 = Ex1.isNumber(num2); // check if num2 is in the valid format
                     int num2_value = Ex1.number2Int(num2); // converting num2 to decimal base (String to Int)
 
-
+                    //let's print what is the status of num2: valid and decimal value
                     System.out.println("num2= " + num2 + " is number: "+ validNum2 + " , value: " + num2_value);
 
-                    if (!validNum2) {
+                    if (!validNum2) // in case of invalid input to num2:
+                    {
+                        // let's restart the loop and ask again for first number, so let's stop this loop iteration and start an new loop iteration:
                         System.out.println("ERR: num2 is in the wrong format! (" + num2+ ")");
                         continue;
                     }
 
-                    System.out.println("Enter a base for output: (a number [2,16]");
-                    int new_Base = sc.nextInt();
-                    if ((new_Base  < 2) || (new_Base > 16))
+                    // moving on to ask the user the desired output number base
+
+                    System.out.println("Enter a base for output: (a number [2,16]"); //msg print to the user
+
+                    int new_Base = sc.nextInt(); //getting base value
+                    if ((new_Base  < 2) || (new_Base > 16)) // invald base value
                     {
+                        //error MSG print
                         System.out.println("ERR: wrong base, should be [2,16], got (" + new_Base + ")");
+                        // let's restart the loop and ask again for first number, so let's stop this loop iteration and start an new loop iteration:
                         continue;
                     }
 
+                    // calculate the sum and product of two numbers (in decimal base):
                     int dec_sum = num1_value + num2_value;
                     int dec_multiplication = num1_value*num2_value;
 
+                    // convert the results to the desired base:
                     String sum_result = Ex1.int2Number(dec_sum, new_Base);
                     String mul_result = Ex1.int2Number(dec_multiplication, new_Base);
 
+                    // printing results in desired base:
                     System.out.println(num1 + " + " + num2 + " = " + sum_result);
                     System.out.println(num1 + " * " + num2 + " = " + mul_result);
 
-                    String [] allNumbersArr = {num1, num2, sum_result, mul_result};
-                    int maxValueIndex = Ex1.maxIndex(allNumbersArr);
+                    // finally let's find the highest value we have in this iteration:
+
+                    String [] allNumbersArr = {num1, num2, sum_result, mul_result}; // putting the numbers of the iteration in an array of Strings
+                    int maxValueIndex = Ex1.maxIndex(allNumbersArr); // using the function maxIndex to find the index of highest value in out array: allNumbersArr
+
+                    // printing the highest value result:
                     System.out.println("Max number over " + Arrays.toString(allNumbersArr) + " is: " + allNumbersArr[maxValueIndex]);
                 }
-                else
             }
         }
+        //out of the loop, so:
         System.out.println("quiting now...");
-        //the program ends...
+        //the program ends.
     }
 }
